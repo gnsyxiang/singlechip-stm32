4dd583b23fe476107df3ace2400514dcacf06f26
zhenquan.qiu <gnsyxiang@163.com>
2019-01-14 10:10:00 +0800

    feat: add gcc tool

--- configs/com-var-def.mk
+++ configs/com-var-def.mk
@@ -18,15 +18,39 @@
 #     last modified: 22/12 2018 17:57
 # ===============================================================
 
+# scale the maximum concurrency with the number of CPUs.
+# # # An additional job is used in order to keep processors busy
+# # # If the number of processors is not available, assume one.
+PARALLEL_JOBS 	:= $(shell echo $$((1 + `getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1`)))
 
-TAR_BZ := tar -xjvf
-TAR_GZ := tar -xzvf
+HOSTMAKE  		:= $(shell which make || echo make)
+MAKE1  			:= $(HOSTMAKE) -j1
+MAKE 			:= $(HOSTMAKE) -j$(PARALLEL_JOBS)
 
-GCC_PATH := tools/gcc-arm-none-eabi-5_4-2016q3/bin/
-GCC_PRE  := arm-none-eabi-
-GCC_TOOL := $(GCC_PATH)/$(GCC_PRE)
+MKDIR 		:= mkdir -p
 
-GCC 	:= $(GCC_TOOL)gcc
-GXX 	:= $(GCC_TOOL)g++
-STRIP 	:= $(GCC_TOOL)strip
+TAR_BZ 		:= tar -xjvf
+TAR_XZ 		:= tar -xvf
+TAR_GZ 		:= tar -xzvf
+
+CAT 		:= cat
+RM 			:= rm -rf
+
+# project structure
+SRC_DIR 	:= src
+INC_DIR 	:= inc
+LIB_DIR 	:= lib
+RES_DIR 	:= res
+OBJ_DIR 	:= obj
+TST_DIR 	:= test
+DEP_DIR 	:= depend-lib
+INS_DIR 	:= install
+
+GCC_PATH 	:= tools/gcc-arm-none-eabi-5_4-2016q3/bin/
+GCC_PRE  	:= arm-none-eabi-
+GCC_TOOL 	:= $(GCC_PATH)/$(GCC_PRE)
+
+GCC 		:= $(GCC_TOOL)gcc
+GXX 		:= $(GCC_TOOL)g++
+STRIP 		:= $(GCC_TOOL)strip
 
